0fb92d976e5c391d4748624c14f5d48f28bf6667
ErjiaGuan <erjia@fb.com>
2020-10-16 18:56:20 -0400

    Implement copysign (fix docs)
    ghstack-source-id: edccfde9095eab988ba254c001b784021fa6fe87
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/46396

--- aten/src/ATen/native/cuda/BinaryMiscOpsKernels.cu
+++ aten/src/ATen/native/cuda/BinaryMiscOpsKernels.cu
@@ -108,6 +108,40 @@ void heaviside_kernel_cuda(TensorIterator& iter) {
   });
 }
 
+template<typename scalar_t, typename accscalar_t>
+struct CopySignScalarFunctor {
+    MulScalarFunctor(accscalar_t b_): b(b_) {}
+    __device__ scalar_t operator() (scalar_t a) const {
+      return ::copysign(a, b);
+    }
+  private:
+    accscalar_t b;
+};
+
+template<typename scalar_t>
+struct CopySignFunctor {
+  __device__ scalar_t operator() (scalar_t a, scalar_t b) const {
+    return ::copysign(a, b);
+  }
+};
+
+void copysign_kernel_cuda(TensorIterator& iter) {
+  if (!isIntegralType(iter.common_dtype(), /*includeBool*/ false) && iter.is_cpu_scalar(2)) {
+    AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES_AND2(kHalf, kBFloat16, iter.common_dtype(), "copysign_cuda", [&]() {
+      using accscalar_t = at::acc_type<scalar_t, true>;
+      auto b = iter.scalar_value<accscalar_t>(2);
+      iter.remove_operand(2);
+      CopySignScalarFunctor<scalar_t, decltype(b)> f(b);
+      gpu_kernel(iter, f);
+    });
+  } else {
+    AT_DISPATCH_ALL_TYPES_AND3(kHalf, kBool, kBFloat16, iter.common_dtype(), "copysign_cuda", [&]() {
+      CopySignFunctor<scalar_t> f;
+      gpu_kernel_with_scalars(iter, f);
+    });
+  }
+}
+
 REGISTER_DISPATCH(atan2_stub, &atan2_kernel_cuda);
 REGISTER_DISPATCH(smooth_l1_stub, &smooth_l1_kernel_cuda);
 REGISTER_DISPATCH(mse_stub, &mse_kernel_cuda);
@@ -118,5 +152,6 @@ REGISTER_DISPATCH(lcm_stub, &lcm_kernel_cuda);
 REGISTER_DISPATCH(hypot_stub, &hypot_kernel_cuda);
 REGISTER_DISPATCH(nextafter_stub, &nextafter_kernel_cuda);
 REGISTER_DISPATCH(heaviside_stub, &heaviside_kernel_cuda);
+REGISTER_DISPATCH(copysign_stub, &copysign_kernel_cuda);
 
 }} // namespace at::native
